2e27d0215ba2a2203de07d11b9ef6b2c6593daac
Thiago_GMota <thiagogabrielmta@outlook.com.br>
2024-06-20 20:23:07 -0300

    primeiro codigo ruby

---
+++ cadastro-user.rb
@@ -0,0 +1,17 @@
+puts "Olá, bem vindo a nossa biblioteca, por favor digite seus dados para finalizarmos sua compra."
+print "Nome: "
+nome = gets.chomp
+
+print "Sobrenome: "
+sobrenome = gets.chomp
+
+print "Cpf: "
+cpf = gets.chomp
+
+print"Idade: "
+idade = gets.chomp
+
+print "Livro escolhido: "
+livroEscolhido = gets.chomp
+
+puts "Olá #{nome} #{sobrenome}, #{livroEscolhido} é um exelente livro, muito obrigado pela compra. Volte sempre "
